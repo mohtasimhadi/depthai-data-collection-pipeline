206f0117b545d2638b62475d16eae8cc93c1f279
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-15 14:44:18 -0500

    Host syncing based on time

--- utils/host_sync.py
+++ utils/host_sync.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+MS_THRESHOLD = 5
+
 class HostSync:
     def __init__(self):
         self.arrays = {}
@@ -23,4 +26,36 @@ class HostSync:
                     else:
                         break
             return synced
+        return False
+
+    def add_msg_t(self, name, msg, ts = None):
+        if ts is None:
+            ts = msg.getTimestampDevice()
+
+        if not name in self.arrays:
+            self.arrays[name] = []
+
+        self.arrays[name].append((ts, msg))
+
+        synced = {}
+        for name, arr in self.arrays.items():
+            diffs = []
+            for i, (msg_ts, msg) in enumerate(arr):
+                diffs.append(abs(msg_ts - ts))
+            if len(diffs) == 0: break
+            diffsSorted = diffs.copy()
+            diffsSorted.sort()
+            dif = diffsSorted[0]
+
+            if dif < timedelta(milliseconds=MS_THRESHOLD):
+                synced[name] = diffs.index(dif)
+
+
+        if len(synced) == 4:
+            for name, i in synced.items():
+                self.arrays[name] = self.arrays[name][i:]
+            ret = {}
+            for name, arr in self.arrays.items():
+                ret[name] = arr.pop(0)[1]
+            return ret
         return False
