716ab7f19df2f6c8ad948b9cdeee60587ac4872c
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-15 15:05:18 -0500

    threading used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os, json
+import time
+import threading
 import depthai as dai
 from camera_setup.pipeline import get_pipeline
 from utils.host_sync import HostSync
@@ -7,81 +9,93 @@ output_dir = 'depthai_output'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-pipeline = get_pipeline()
+def main(thread):
 
-with dai.Device(pipeline) as device:
-    queues = []
-    queues.append(device.getOutputQueue(name="depth", maxSize=30, blocking=True))
-    queues.append(device.getOutputQueue(name="color", maxSize=30, blocking=True))
-    queues.append(device.getOutputQueue(name="monoL", maxSize=30, blocking=True))
-    queues.append(device.getOutputQueue(name="monoR", maxSize=30, blocking=True))
-    queues.append(device.getOutputQueue(name="imu", maxSize=30, blocking=True))
-    imu_queue = device.getOutputQueue(name='imu', maxSize=30, blocking=True)
-    sync = HostSync()
+    pipeline = get_pipeline()
 
-    calibData = device.readCalibration()
-    
-    calib_dict = {
-        'left_intrinsics'           : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, dai.Size2f(1080, 720)),
-        'right_intrinsics'          : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_C, dai.Size2f(1080, 720)),
-        'rgb_intrinsics'            : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_A, dai.Size2f(1920, 1080)),
-        'left_distortion'           : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_B),
-        'right_distortion'          : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_C),
-        'rgb_distortion'            : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_A),
-        'extrinsics_left_to_right'  : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_B, dai.CameraBoardSocket.CAM_C),
-        'extrinsics_right_to_left'  : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_C, dai.CameraBoardSocket.CAM_B),
-        'extrinsics_left_to_rgb'    : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_B, dai.CameraBoardSocket.CAM_A),
-        'extrinsics_right_to_rgb'   : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_C, dai.CameraBoardSocket.CAM_A)
-    }
-    file_calibration = os.path.join(output_dir, 'calibration.json')
-    with open(file_calibration, 'w') as f:
-        json.dump(calib_dict, f, indent=4)
+    with dai.Device(pipeline) as device:
+        queues = []
+        queues.append(device.getOutputQueue(name="depth", maxSize=30, blocking=True))
+        queues.append(device.getOutputQueue(name="color", maxSize=30, blocking=True))
+        queues.append(device.getOutputQueue(name="monoL", maxSize=30, blocking=True))
+        queues.append(device.getOutputQueue(name="monoR", maxSize=30, blocking=True))
+        queues.append(device.getOutputQueue(name="imu", maxSize=30, blocking=True))
+        imu_queue = device.getOutputQueue(name='imu', maxSize=30, blocking=True)
+        sync = HostSync()
 
-    imu_list = []
-    # baseTs = None
+        calibData = device.readCalibration()
+        
+        calib_dict = {
+            'left_intrinsics'           : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, dai.Size2f(1080, 720)),
+            'right_intrinsics'          : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_C, dai.Size2f(1080, 720)),
+            'rgb_intrinsics'            : calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_A, dai.Size2f(1920, 1080)),
+            'left_distortion'           : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_B),
+            'right_distortion'          : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_C),
+            'rgb_distortion'            : calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_A),
+            'extrinsics_left_to_right'  : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_B, dai.CameraBoardSocket.CAM_C),
+            'extrinsics_right_to_left'  : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_C, dai.CameraBoardSocket.CAM_B),
+            'extrinsics_left_to_rgb'    : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_B, dai.CameraBoardSocket.CAM_A),
+            'extrinsics_right_to_rgb'   : calibData.getCameraExtrinsics(dai.CameraBoardSocket.CAM_C, dai.CameraBoardSocket.CAM_A)
+        }
+        file_calibration = os.path.join(output_dir, 'calibration.json')
+        with open(file_calibration, 'w') as f:
+            json.dump(calib_dict, f, indent=4)
 
-    file_color = open(os.path.join(output_dir, 'color.h265'), 'wb')
-    file_monoL = open(os.path.join(output_dir, 'monoL.h264'), 'wb')
-    file_monoR = open(os.path.join(output_dir, 'monoR.h264'), 'wb')
-    file_depth = open(os.path.join(output_dir, 'depth.mjpeg'), 'wb')
-    file_times = open(os.path.join(output_dir, 'timestamps.txt'), 'wb')
-    file_imus = open(os.path.join(output_dir, 'imu_data.txt'), 'wb')
-    print("Press Ctrl+C to stop encoding...")
-    try:
-        while True:
-            imu_message = imu_queue.get()
-            if imu_message is not None:
-                imu_packets = imu_message.packets
-                for imu_packet in imu_packets:
-                    acceleroValues = imu_packet.acceleroMeter
-                    gyroValues = imu_packet.gyroscope
-                    acceleroTs = acceleroValues.getTimestampDevice()
-                    gyroTs = gyroValues.getTimestampDevice()
+        file_color = open(os.path.join(output_dir, f'{thread}_color.h265'), 'wb')
+        file_monoL = open(os.path.join(output_dir, f'{thread}_monoL.h264'), 'wb')
+        file_monoR = open(os.path.join(output_dir, f'{thread}_monoR.h264'), 'wb')
+        file_depth = open(os.path.join(output_dir, f'{thread}_depth.mjpeg'), 'wb')
+        file_times = open(os.path.join(output_dir, f'{thread}_timestamps.txt'), 'wb')
+        file_imus  = open(os.path.join(output_dir, f'{thread}_imu_data.txt'), 'wb')
+        print("Press Ctrl+C to stop encoding...")
+        try:
+            while True:
+                imu_message = imu_queue.get()
+                if imu_message is not None:
+                    imu_packets = imu_message.packets
+                    for imu_packet in imu_packets:
+                        acceleroValues = imu_packet.acceleroMeter
+                        gyroValues = imu_packet.gyroscope
+                        acceleroTs = acceleroValues.getTimestampDevice()
+                        gyroTs = gyroValues.getTimestampDevice()
 
-                    imu_data = {
-                        "acceleroMeter" : {
-                            "x": acceleroValues.x,
-                            "y": acceleroValues.y,
-                            "z": acceleroValues.z,
-                            "timestamp": acceleroTs
-                        },
-                        "gyroscope"     : {
-                            "x": gyroValues.x,
-                            "y": gyroValues.y,
-                            "z": gyroValues.z,
-                            "timestamp": gyroTs
+                        imu_data = {
+                            "acceleroMeter" : {
+                                "x": acceleroValues.x,
+                                "y": acceleroValues.y,
+                                "z": acceleroValues.z,
+                                "timestamp": acceleroTs
+                            },
+                            "gyroscope"     : {
+                                "x": gyroValues.x,
+                                "y": gyroValues.y,
+                                "z": gyroValues.z,
+                                "timestamp": gyroTs
+                            }
                         }
-                    }
-                    file_imus.write(("{'imu_timestamp': '" + str(imu_message.getTimestampDevice()) + "',    'IMU': " + str(imu_data) + '\n').encode())
+                        file_imus.write(("{'imu_timestamp': '" + str(imu_message.getTimestampDevice()) + "',    'IMU': " + str(imu_data) + '\n').encode())
 
-            for queue in queues:
-                new_message = queue.get()
-                message = sync.add_msg(queue.getName(), new_message)
-                if message:
-                    file_depth.write(message['depth'].getData())
-                    file_color.write(message['color'].getData())
-                    file_monoL.write(message['monoL'].getData())
-                    file_monoR.write(message['monoR'].getData())
-                    file_times.write(("{'camera_timestamp': '" + str(message['depth'].getTimestampDevice()) + "', 'frame_number': " + str(message['depth'].getSequenceNum())+"}\n").encode())
-    except KeyboardInterrupt:
-        pass
+                for queue in queues:
+                    new_message = queue.get()
+                    message = sync.add_msg(queue.getName(), new_message)
+                    if message:
+                        file_depth.write(message['depth'].getData())
+                        file_color.write(message['color'].getData())
+                        file_monoL.write(message['monoL'].getData())
+                        file_monoR.write(message['monoR'].getData())
+                        file_times.write(("{'camera_timestamp': '" + str(message['depth'].getTimestampDevice()) + "', 'frame_number': " + str(message['depth'].getSequenceNum())+"}\n").encode())
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == "__main__":
+    thread_01 = threading.Thread(target=main, args=('thread_01', ))
+    thread_02 = threading.Thread(target=main, args=('thread_02', ))
+    thread_01.start()
+    time.sleep(2)
+    thread_02.start()
+    
+    thread_01.join()
+    thread_02.join()
+
+    print("All recordings stopped.")
