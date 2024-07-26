2554487f5e1a28094135a63a6652e9fffa831479
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-26 14:38:28 -0500

    publisher/subscriber module initial

--- utils/logs.py
+++
@@ -1,9 +0,0 @@
-RED         = '\033[91m'
-GREEN       = '\033[92m'
-BLUE        = '\033[94m'
-YELLOW      = '\033[93m'
-RESET       = '\033[0m'
-ERROR       = RED    +  "[Error]    "   +RESET
-WARNING     = YELLOW +  "[Warning]  "   +RESET
-LOG         = BLUE   +  "[Log]      "   +RESET
-SUCCESS     = GREEN  +  "[Success]  "   +RESET
